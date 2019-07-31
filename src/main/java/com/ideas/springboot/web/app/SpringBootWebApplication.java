0ed119ec5c9769bca83ea606c8b557ff9e3b54ac
israel <israel.bejarano.a@gmail.com>
2019-07-31 16:02:47 +0200

    add javadoc

--- src/main/java/com/ideas/springboot/web/app/SpringBootWebApplication.java
+++ src/main/java/com/ideas/springboot/web/app/SpringBootWebApplication.java
@@ -3,9 +3,17 @@ package com.ideas.springboot.web.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The Class SpringBootWebApplication.
+ */
 @SpringBootApplication
 public class SpringBootWebApplication {
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebApplication.class, args);
 	}
